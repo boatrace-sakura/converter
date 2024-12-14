16786499acf1bbc3f9517c3842776d4385826f83
shimomo <developer@shimomo.net>
2024-12-14 21:05:17 +0900

    Use spread operator

--- src/Converter.php
+++ src/Converter.php
@@ -35,7 +35,7 @@ class Converter
      */
     public function __call(string $name, array $arguments): string|float|int|null
     {
-        return call_user_func_array([$this->converter, $name], $arguments);
+        return $this->converter->$name(...$arguments);
     }
 
     /**
@@ -45,7 +45,7 @@ class Converter
      */
     public static function __callStatic(string $name, array $arguments): string|float|int|null
     {
-        return call_user_func_array([self::getInstance(), $name], $arguments);
+        return self::getInstance()->$name(...$arguments);
     }
 
     /**
